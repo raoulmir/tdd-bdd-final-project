2b6d23762bb22c0df28a0ee540a036854eef0339
raoulmir <mironraoul@gmail.com>
2025-01-07 10:42:26 -0500

    Tweak update product test logic

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -193,13 +193,14 @@ class TestProductRoutes(TestCase):
     def test_update_product(self):
         test_product = ProductFactory()
 
-        response = self.client.post(BASE_URL, Product.serialize(test_product))
-        self.assertEqual(response.status_code, HTTP_201_CREATED
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
         new_product = response.get_json()
-        new_product["description"] = 'unknown'
+        new_product['description'] = 'unknown'
     
-        response = self.client.put(BASE_URL, json={new_product})
-        self.assertEqual(update.status, HTTP_200_OK)
+        response = self.client.put(BASE_URL, json=new_product)
+        self.assertEqual(update.status, status.HTTP_200_OK)
         updated_product = response.get_json()
         self.assertEqual(updated_product["description"], 'unknown')
 
