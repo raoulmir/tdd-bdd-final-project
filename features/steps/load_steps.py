e26f3548d22f5eb181a9434b65cae08523802dc2
raoulmir <mironraoul@gmail.com>
2025-01-08 15:48:12 -0500

    Update request variable name reference and update incorrect HTTP status assertion

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -54,8 +54,8 @@ def step_impl(context):
             "available": row['available'] in ['True', 'true', 1],
             "category": row['category']
         }
-        context.resp = request.post(
+        context.resp = requests.post(
             rest_endpoint,
             json=payload
         )
-        assert context.resp.status_code == HTTP_200_OK
+        assert context.resp.status_code == HTTP_201_CREATED
